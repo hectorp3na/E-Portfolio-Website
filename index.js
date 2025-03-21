a2182ec24a37186f518638f1285fb8e6945b4b8d
Hector <hectorpena6590@gmail.com>
2025-03-21 00:07:15 -0700

    contact form

---
+++ index.js
@@ -0,0 +1,26 @@
+// template_nqz0v2i
+// service_n3wdw77
+// fAObXu_2oKBt1NaXA
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector('.modal__overlay--loading');
+  const success = document.querySelector('.modal__overlay--success');
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      'service_n3wdw77',
+      'template_nqz0v2i',
+      event.target,
+      'fAObXu_2oKBt1NaXA'
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    }).catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contatct me directly at hectorpena6590@gmail.com"
+    )
+   });
+    }
